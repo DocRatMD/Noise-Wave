ead9e0a1321ec002965ac6034a3873e6c33f1a04
DocRatMD <47792236+DocRatMD@users.noreply.github.com>
2019-04-02 00:26:22 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,6 +16,7 @@ function draw() {
   let xoff = 0.0;
   for (let x=0; x<=width; x+=10) {
     let y = map(noise(xoff,yoff),0,1,200,300);
+    strokeWeight(2);
     stroke(random(0,255),random(0,255),random(0,255));
     vertex(x,y);
     point(x,y);
@@ -23,7 +24,7 @@ function draw() {
   }
   yoff += 0.01;
   fill(255);
-  vertex(width+70, height);
-  vertex(0, height);
+  vertex(width+70, height+10);
+  vertex(0, height+10);
   endShape(CLOSE);
 }
