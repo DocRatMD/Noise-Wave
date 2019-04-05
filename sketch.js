2c9a2bf514a880fa380196c6716e5dcd12c89df9
DocRatMD <47792236+DocRatMD@users.noreply.github.com>
2019-04-04 20:19:22 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,81 +2,15 @@ let yoff = 0.0;
 let y;
 let r,g,b;
 let rdir,gdir,bdir;
+let myWave;
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  fill(255);
-  r = random(0,255);
-  g = random(0,255);
-  b = random(0,255);
-  rdir = true;
-  gdir = true;
-  bdir = true;
+  myWave = new wave();
+  myWave.initialize();
 }
 
 function draw() {
   background(51);
-
-  //RED
-  if (r <= 0) {
-    rdir = true;
-  }
-  if (r >= 255) {
-    rdir = false;
-  }
-
-  if (rdir == true) {
-    r += 1;
-  }
-
-  if(rdir == false) {
-    r -= 1;
-  }
-
-  //GREEN
-  if (g <= 0) {
-    gdir = true;
-  }
-  if (g >= 255) {
-    gdir = false;
-  }
-
-  if (gdir == true) {
-    g += 0.8;
-  }
-
-  if(gdir == false) {
-    g -= 0.8;
-  }
-
-  //BLUE
-  if (b <= 0) {
-    bdir = true;
-  }
-  if (r >= 255) {
-    bdir = false;
-  }
-
-  if (bdir == true) {
-    b += 1.2;
-  }
-
-  if(bdir == false) {
-    b -= 1.2;
-  }
-
-  beginShape();
-
-  let xoff = 0.0;
-  for (let x=0; x<=width; x+=10) {
-    let y = map(noise(xoff,yoff),0,1,height/3,height/2);
-    strokeWeight(5);
-    stroke(r,g,b);
-    vertex(x,y);
-    point(x,y);
-    xoff += 0.05;
-  }
-  yoff += 0.01;
-  vertex(width+70, height+10);
-  vertex(-25, height+10);
-  endShape(CLOSE);
+  myWave.col();
+  myWave.show();
 }
